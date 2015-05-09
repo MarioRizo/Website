0b84d00a18eb4c716de7809e21b4fa4ec9a8e891
MarioER <Marioluigi_36@hotmail.com>
2015-05-09 16:42:58 -0500

    organizing

--- first-site/js/randimg.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  var bgArray = ['bar.jpg', 'building.jpg', 'entrancefloor.jpg', 'morefood.jpg', 'frontbench.jpg', 'moreart.jpg', 'morebar.jpg', 'tortilliera.jpg'];
-  var bg = bgArray[Math.floor(Math.random() * bgArray.length)];
-  var path = 'images/';
-  var imageUrl = path + bg;
-  $('body').css('background-image', 'url(' + imageUrl + ')');
-});
-
